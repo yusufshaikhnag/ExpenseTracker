2a0f900ffd066240975374c21c872ec5a3a5d4ab
yusufshaikhnag <yusuf.shaikhnag@gmail.com>
2023-07-20 19:04:35 +0530

    feat:added screens and styles

---
+++ screens/ManageExpense.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+
+function ManageExpense() {
+    return <Text>ManageExpense Screen</Text>;
+    
+}
+
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+
+function RecentExpenses() {
+    return <ExpensesOutput expensesPeriod="Last 7 days"/>;
+    
+}
+
+
+export default RecentExpenses;

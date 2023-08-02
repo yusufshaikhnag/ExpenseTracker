2af5e2e9724653162bddb8ecf32d44da2193f41a
yusufshaikhnag <yusuf.shaikhnag@gmail.com>
2023-08-02 18:53:05 +0530

    feat:custominputcomponent

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Input from './Input';
+
+const ExpenseForm = () => {
+    const amountChangeHandler = () => {}
+  return (
+  <View style={styles.form}>
+  <Text style={styles.title}>Your Expense</Text>
+  <View style={styles.inputsRow}>
+  <Input 
+  style={styles.rowInput}
+  label="Amount" 
+  textInputConfig={{
+        KeyboardType: 'decimal-pad',
+        onChangeText:  amountChangeHandler,
+    }}/>
+    <Input 
+    style={styles.rowInput}
+    label="Date" 
+    textInputConfig={{
+        placeholder: 'YYYY-MM-DD',
+        maxLength: 10,
+        onChangeText: () => {}
+    }}/>
+  </View>
+  
+    <Input label="Description" textInputConfig={{
+        multiline: true,
+    }}/>
+  </View>
+  );
+  
+}
+
+export default ExpenseForm; 
+
+const styles = StyleSheet.create({
+    form: {
+      marginTop: 40,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+      marginVertical: 24,
+      textAlign: 'center'
+    },
+    inputsRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    rowInput: {
+      flex: 1,
+    },
+  });
